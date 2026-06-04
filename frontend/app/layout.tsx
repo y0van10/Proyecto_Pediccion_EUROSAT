5a03fa56e4b09759863e26b3a408fc58844114d3
Estudiante UNA <estudiante@unap.edu.pe>
2026-06-04 11:57:07 -0500

    fix: change toaster theme to system to resolve nextjs compile type error

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
         <main className="relative z-10 max-w-7xl mx-auto px-4 py-6 sm:px-6 lg:px-8">
           {children}
         </main>
-        <Toaster position="top-center" theme="inherit" richColors closeButton />
+        <Toaster position="top-center" theme="system" richColors closeButton />
       </body>
     </html>
   );
